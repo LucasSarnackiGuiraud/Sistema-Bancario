02268c851b258812657acfffad1093b83343c498
joaozanini <joaozanini.pessoal@gmail.com>
2025-08-13 20:01:13 -0300

    Update TableModelCliente.java

--- Sistema Bancario/src/util/TableModelCliente.java
+++ Sistema Bancario/src/util/TableModelCliente.java
@@ -3,4 +3,9 @@ package util;
 import javax.swing.table.AbstractTableModel;
 
 public class TableModelCliente extends AbstractTableModel {
+
+    // essa classa vai parar de dar erro qnd usarmos um dos métodos:
+    // getRowCount()
+    // getColumnCount()
+    // getValueAt()
 }
